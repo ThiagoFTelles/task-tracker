e809d61904a469b6077a606718dc9eae7f0756d8
ThiagoFTelles <atendimento.telles@gmail.com>
2023-03-03 12:50:56 -0300

    feat: implementa controle dos botões

--- src/components/TemporizadorFormulario.vue
+++ src/components/TemporizadorFormulario.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="is-flex is-align-items-center is-justify-content-space-between">
           <CronometroFormulario :tempo-em-segundos="tempoEmSegundos" />
-          <button class="button" @click="iniciarContagem">
+          <button class="button" @click="iniciarContagem" :disabled="cronometroRodando">
             <span class="icon">
               <i class="fas fa-play"></i>
             </span>
             <span>play</span>
           </button>
-          <button class="button" @click="finalizarContagem">
+          <button class="button" @click="finalizarContagem" :disabled="!cronometroRodando">
             <span class="icon">
               <i class="fas fa-stop"></i>
             </span>
@@ -27,15 +27,18 @@ export default defineComponent({
     return {
       tempoEmSegundos: 0,
       cronometro: 0,
+      cronometroRodando: false,
     };
   },
   methods: {
     iniciarContagem(): void {
       this.cronometro = setInterval(() => { this.tempoEmSegundos += 1; }, 1000);
+      this.cronometroRodando = true;
     },
     finalizarContagem(): void {
       clearInterval(this.cronometro);
       this.tempoEmSegundos = 0;
+      this.cronometroRodando = false;
     },
   },
 });
