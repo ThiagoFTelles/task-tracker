448e11cce9f89f8b11ba5947f980158612c68af8
ThiagoFTelles <atendimento.telles@gmail.com>
2023-03-03 11:07:50 -0300

    feat: adicona FA

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
+// eslint-disable-next-line import/no-extraneous-dependencies
+import '@fortawesome/fontawesome-free/css/all.css';
+
 createApp(App).mount('#app');
